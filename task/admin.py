f27910dfac2674a139020499ca5ba825c056b782
Timofey Vetrov <Elrynishe@yandex.ru>
2025-05-03 18:05:03 +0300

    admin

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import Task, Board_Column
 # Register your models here.
+@admin.register(Board_Column)
+class Board_Column_Admin(admin.ModelAdmin):
+    list_display = ('name', 'position')
+    ordering = ('position',)
+
+@admin.register(Task)
+class Task_Admin(admin.ModelAdmin):
+    list_display = ('title', 'assignee', 'status', 'priority', 'due_date', 'created_at')
+    list_filter = ('status', 'priority', 'due_date')
+    search_fields = ('title', 'description')
+    ordering = ('due_date',)
