0f9c8e5773a1847b8276a7b182d0b334f8da52b1
Timofey Vetrov <Elrynishe@yandex.ru>
2025-05-09 00:08:03 +0300

    html

--- task/views.py
+++ task/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Board_Column
 # Create your views here.
+
+def kanban_board(request):
+    columns = Board_Column.objects.prefetch_related('task_set').order_by('position')
+    return render(request, 'tasks/kanban_board.html', {'columns': columns})
